bad16a681ffe11fc71010e219ac1d61dab34c349
kornatskyi <bohdan.kornatskyi@gmail.com>
2021-08-14 12:11:13 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '559974871914976',
+        clientSecret: '71e43913ed38d00581bfbc79eb1c68db'
+    }
 }
